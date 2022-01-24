68177ea77f217027ac517a1cbb33ebb967b4e46c
MarcinSlusarczyk <83789546+MarcinSlusarczyk@users.noreply.github.com>
2022-01-24 18:00:23 +0100

    Update oferty_pracy_bot.py

--- oferty_pracy_bot.py
+++ oferty_pracy_bot.py
@@ -11,10 +11,12 @@ import schedule
 import datetime
 
 
+
+
 stanowisko = ('finanse-ksiegowosc', 'administracja-biurowa')
 
 gmail_address = 'marcin.automatyzacje@gmail.com'
-password = 'P123'
+password = 'Pyt123'
 
 def send_email_alert_new(link, title):
     try:
@@ -41,16 +43,16 @@ def send_email_alert_new(link, title):
     except:
         print("błąd wysyłania")
         
-oferty = {}
 
-# POBIERANIE Z PRACUJ.PL
+
+
 def main():
     try:
         
         for st in stanowisko:
             site = f'https://www.pracuj.pl/praca/{st};kw/dzierzoniow;wp?rd=5'
 
-            page = requests.get(site)
+            page = requests.get(site, timeout=60)
             soup = BeautifulSoup(page.content, "html.parser")
 
             for orders in soup.find_all('li', class_='results__list-container-item'):
@@ -59,15 +61,17 @@ def main():
                     title = orders.find('a', class_="offer-details__title-link").text
                     link = orders.find('a', class_="offer-details__title-link")['href']
                     
-                    if title not in oferty:
-                        oferty[title] = link
-                        send_email_alert_new(link, title)
-                        print(f'wysyłam mail z pracuj.pl: {title}')
+                    with open('oferty.csv', 'a+', encoding='UTF8') as file:
+                        file.seek(0)                                          
+                        if title not in file.read():                            
+                            file.write(f'{title}; {link}\n') # in append mode writes will always go to the end, so no need to seek()
+                            send_email_alert_new(link, title)
+                            print(f'wysyłam mail z pracuj: {title}')
                 except:
                     pass
         
 
-        # POBIERANIE Z OLX
+        
         
         for st in stanowisko:
             
@@ -75,26 +79,30 @@ def main():
                 
                 site = f'https://www.olx.pl/praca/{st}/dzierzoniow/?search%5Bdist%5D=5/?page={page_nr}'
 
-                page = requests.get(site)
+                page = requests.get(site, timeout=60)
                 soup = BeautifulSoup(page.content, "html.parser")
 
                 for orders in soup.find_all('div', class_='offer-wrapper'):
                     
                     title=orders.find('strong').text.strip()
-                    link = orders.find('a')['href']        
+                    link = orders.find('a')['href']     
                     
-                    if title not in oferty:
-                        oferty[title]= link
-                        send_email_alert_new(link, title)
-                        print(f'wysyłam mail z olx: {title}')
-        
-            
-        print(f'ilość ofert pracy na pracuj.pl i olx: {len(oferty)} -- {datetime.datetime.now()}')
-    except:
-        print("błąd sieciowy")
+                    with open('oferty.csv', 'a+', encoding='UTF8') as file:
+                        file.seek(0)
+                                          
+                        if title not in file.read():                            
+                            file.write(f'{title}; {link}\n') # in append mode writes will always go to the end, so no need to seek()
+                            send_email_alert_new(link, title)
+                            print(f'wysyłam mail z olx: {title}')
+          
+        print(f'Progam działa -- {datetime.datetime.now()}')
     
-schedule.every(10).seconds.do(main)
+    except Exception as err:
+        print(err)
+    
+schedule.every(30).seconds.do(main)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
